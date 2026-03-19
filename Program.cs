0cc60324770dfb9c4cd65b064a45062a8224772e
charan k <charancr1630@gmail.com>
2026-03-19 23:29:05 +0530

    added crud operations with a file as backup

--- Program.cs
+++ Program.cs
@@ -1,39 +1,110 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.CommandLine;
+using System.Reflection.Metadata;
+using System.Text.Json;
 using Webmarkers;
-WebMarkerServices webmark = new WebMarkerServices();
-Console.WriteLine("Welcome to Webmarker  o(*^＠^*)o");
 
+WebMarkerServices _webmark = new WebMarkerServices();
+Console.WriteLine("Welcome to Webmarker  o(*^＠^*)o");
+//settting up default storage path for json file
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "WebMarker.json");
+FileInfo JsonFile = new FileInfo(filePath);
 var rootcommand = new RootCommand("a tool to add your web markings");
 
 var nameoption = new Option<string[]>("--name", "-n") { Description = "takes the website name", Required = true,AllowMultipleArgumentsPerToken=true };
 var urloption = new Option<string[]>("--url", "-u") { Description = "takes the website url", Required = true, AllowMultipleArgumentsPerToken = true };
-var categoryoption = new Option<string[]>("--category", "-c") { Description = "takes the website catogory", Required = true, AllowMultipleArgumentsPerToken = true };
-categoryoption.AcceptOnlyFromAmong("movie websites", "learning websites", "anime websites");
+var categoryoption = new Option<string[]>("--category", "-c") { Description = "takes the website catogory", Required = false, AllowMultipleArgumentsPerToken = true };
+categoryoption.AcceptOnlyFromAmong("movie", "learning", "anime");
+var idOption = new Option<int>("--id", "-i");
+//var fileinputoption = new Option<FileInfo>("--file", "-f") { DefaultValueFactory=(_)=> { return file; } };
+
+urloption.Validators.Add(result =>
+{
+    foreach (var token in result.Tokens)
+    {
+        if (string.IsNullOrWhiteSpace(token.Value))
+        {
+            result.AddError("url cannot be empty");
+            break;
+        }
+        else if (!Uri.TryCreate(token.Value, UriKind.Absolute, out _)) {
+            result.AddError($"Invalid url:{token.Value}");
+            break;                                
+        
+        }
+    }
+});
 
 
 var linkcommand = new Command("link", "used to perform operatios on links");
 var addcommand = new Command("add", "used to add the web mark") {
 nameoption,
 urloption,
-categoryoption
+categoryoption,
+
 };
+var removeCommand = new Command("remove", "remove the webmark") {
+    idOption
+
+};
+var updateCommand = new Command("update", "update the url") { idOption,urloption };
+
+var listcommand = new Command("list", "used to list all the bookmarks") { 
+    categoryoption
+};
+var importcommand = new Command("import", "importing the bookmarks") {
+
+};
+
 
 rootcommand.Subcommands.Add(linkcommand);
 linkcommand.Subcommands.Add(addcommand);
-
+linkcommand.Subcommands.Add(importcommand);
+linkcommand.Subcommands.Add(listcommand);
+linkcommand.Subcommands.Add(removeCommand);
+linkcommand.Subcommands.Add(updateCommand);
 ParseResult parseResult = rootcommand.Parse(args);
+
+listcommand.SetAction((ParseResult) => {
+    string[] category =parseResult.GetValue(categoryoption);
+    if (File.Exists(JsonFile.FullName))
+    {
+        _webmark.FileReader(JsonFile);
+    }
+    _webmark.ListWebMarker(category); 
+
+});
+
 
 addcommand.SetAction((parseResult) => {
     string[] name = parseResult.GetValue(nameoption);
     string[] url = parseResult.GetValue(urloption);
     string[] category = parseResult.GetValue(categoryoption);
-    webmark.AddWebMarker(name,url, category);
 
-    var Listofwebmarks=webmark.ListWebMarker();
-    foreach (var webmark in Listofwebmarks)
+    _webmark.FileReader(JsonFile);
+    _webmark.AddWebMarker(name, url, category);
+_webmark.FileWriter(JsonFile);
+
+});
+removeCommand.SetAction((ParseResult) =>
+{
+    int idval = ParseResult.GetValue(idOption);
+    if (idval != 0)
     {
-        Console.WriteLine($"{webmark.WebName} | {webmark.WebUrl} |{webmark.Category}");
+        _webmark.FileReader(JsonFile);
+        _webmark.RemoveWebMarker(idval);
+        _webmark.FileWriter(JsonFile);
+    }
+});
+
+updateCommand.SetAction((ParseResult) => {
+    int idval = ParseResult.GetValue(idOption);
+    string[] url=parseResult.GetValue(urloption);
+    if (idval != 0)
+    {
+        _webmark.FileReader(JsonFile);
+        _webmark.UpdateWebMarker(idval, url);
+        _webmark.FileWriter(JsonFile);
     }
 
 });
--- WebMarkerServices.cs
+++ WebMarkerServices.cs
@@ -1,29 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+
 
 namespace Webmarkers
 {
     public class WebMarkerServices
     {
         public List<WebMarker> webMarkers = new();
-
-        public void AddWebMarker(string webname,string url,string category) {
-            webMarkers.Add(new WebMarker(webname, url,category));
-        }
+        
         public void AddWebMarker(string[] name, string[] url, string[] category)
         {
+            //linq query is to get the names of webmarks
+            IEnumerable<string> Markers = from x in webMarkers
+                                             select x.WebName;
+            
+
             for (int i = 0; i < name.Length; i++)
             {
+                if (!Markers.Contains(name[i])) { 
                 webMarkers.Add(new WebMarker(name[i], url[i], category[i]));
+                    Console.WriteLine("Added sucessfully");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You cannot enter the same bookmark again");
+                    Console.ResetColor();
+                }
+
 
             }
+            
         }
-        public void RemoveWebMarker(int index) { webMarkers.RemoveAt(index); }
+        public void RemoveWebMarker(int index) {
 
-        public List<WebMarker> ListWebMarker()
+            webMarkers.RemoveAt(index-1); 
+
+        
+        
+        }
+
+        public void ListWebMarker(string[] category)
         {
-            return webMarkers;
+            if (category.Length>0) {
+                IEnumerable <WebMarker> result= from m in webMarkers
+                                                 where m.Category == category[0]
+                                                 select m;
+                webMarkers = result.ToList();
+
+            }
+            int count = 0;
+            Console.ForegroundColor= ConsoleColor.Green;
+            foreach (WebMarker mark in webMarkers)
+            {
+                count++;
+                Console.WriteLine($"{count}) {mark.WebName} | {mark.WebUrl} |{mark.Category}");
+            }
+            Console.ResetColor();
+                
 
         }
         public void EditWebMarker(WebMarker webMarker,string title,string url,string category) {
@@ -32,5 +68,38 @@ namespace Webmarkers
             webMarker.Category=category;
         }
 
+        public void ImportWebMarker(List<WebMarker> _webMarker) {
+            int count = 0;
+            foreach (WebMarker webMarker in _webMarker) {
+                webMarkers.Add(new WebMarker(webMarker.WebName,webMarker.WebUrl,webMarker.Category));
+                count++;
+                Console.WriteLine($"{count}) {webMarker.WebName} | {webMarker.WebUrl} |{webMarker.Category}");
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{count} webmarks are added");
+            Console.ResetColor();
+
+        } 
+
+        public void FileReader(FileInfo file)
+        {
+            var content = File.ReadAllText(file.FullName);
+            if (!string.IsNullOrEmpty(content))
+            {
+                List<WebMarker> Markers = JsonSerializer.Deserialize<List<WebMarker>>(content);
+                webMarkers = Markers;
+            }
+        }
+
+        public void FileWriter(FileInfo file)
+        {
+            var json=JsonSerializer.Serialize(webMarkers, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(file.FullName, json);
+        }
+
+        public void UpdateWebMarker(int idval,string[] url)
+        {
+            webMarkers[idval - 1].WebUrl= url[0];
+        }
     }
 }
